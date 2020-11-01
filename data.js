280465be327571bb60af738f3c4a1fd193fb8edf
PedroB <pedrobarragan30@gmail.com>
2020-11-01 01:58:44 -0600

    Adding first version of POM, data and module_Account files

---
+++ data.js
@@ -0,0 +1,19 @@
+
+let rndNumber = Math.random().toString(36).substr(1,4);
+
+let data = {
+    email: 'dummy' + rndNumber + '@mailinator.com',
+    bad_email: "bad@mail",
+    firstName: "Juan",
+    lastName: "Camaney",
+    password: "123456",
+    pbox: "Somewhere underthe Rainbow 1234",
+    city: "Mobile",
+    state: "Alabama",
+    zipCode: "36602",
+    country: "United States",
+    mobilePhone: "219892828492",
+    alias : "My Home"
+}
+
+export {data}
---
+++ module_Account.js
@@ -0,0 +1,138 @@
+import page from './POM';
+import { data } from './data';
+
+fixture('Performing Tests over Account Module')
+    .page ('http://automationpractice.com');
+
+test('Leave Email address empty and click on Create an account, expect an error', async t =>
+{
+    await t
+        //Maximizing the Screen
+        .maximizeWindow()
+        .click(page.lnk_SignIn)
+
+    await t
+        //Leave Email address empty and click on 'Create an account' 
+        .click(page.btn_CreateAccount)
+
+    
+    await t
+        //Expecting an error message
+        .expect(await page.msg_CreateAccountError01.innerText).eql("Invalid email address.")
+        .expect(await page.msg_CreateAccountError01.exists).ok()
+        .takeScreenshot()
+});
+
+test('Enter an Invalid Email address and click on Create an account, expect an error', async t =>
+{
+    await t
+        //Maximizing the Screen
+        .maximizeWindow()
+        .click(page.lnk_SignIn)
+
+    
+    await t
+        //Enter an Invalid Email address and click on 'Create an account'
+        .typeText(page.txt_email, data.bad_email)
+        .click(page.btn_CreateAccount)
+    
+    await t
+        //Expecting an error message
+        .expect(await page.msg_CreateAccountError01.innerText).eql("Invalid email address.")
+        .expect(await page.msg_CreateAccountError01.exists).ok()
+        .takeScreenshot()
+});
+
+test('Create an account without filling any requried field, expect multiple error messages', async t =>
+{
+    await t
+        //Maximizing the Screen
+        .maximizeWindow()
+        .click(page.lnk_SignIn)
+
+    await t
+        //Enter a valid Email address and click on 'Create an account'
+        .typeText(page.txt_email, data.email)
+        .click(page.btn_CreateAccount)
+
+    await t
+        //Leave all fields empty and click on 'Register', expecting multiple error messages
+        .click(await page.btn_register)
+
+    await t
+        //Expecting an error message
+        .expect(await page.msg_CreateAccountError02.innerText).eql("There are 8 errors")
+        .expect(await page.msg_CreateAccountError02.exists).ok()
+        .takeScreenshot()
+});
+
+
+test('Successfully create an account', async t =>
+{
+    await t
+        //Maximizing the Screen
+        .maximizeWindow()
+        .click(page.lnk_SignIn)
+    
+    await t
+        //Enter a valid Email address and click on 'Create an account'
+        .typeText(page.txt_email, data.email)
+        .click(page.btn_CreateAccount)
+        
+    await t 
+        //Validate that email is  prefilled and it's editable
+        .expect(page.txt_frm_email.value).contains(data.email)
+        .expect(page.txt_frm_email.hasAttribute('readonly')).notOk()
+
+        //Writing First and Last Name
+        .typeText(page.txt_frm_firstName, data.firstName)
+        .typeText(page.txt_frm_lastName, data.lastName)
+
+        //Checking First and Last Name are automatically filled in address field
+        .expect(page.txt_frm_firstName_Address.value).contains(data.firstName)
+        .expect(page.txt_frm_lastName_Address.value).contains(data.lastName)
+
+        //Writing Password and Address
+        .typeText(page.txt_frm_password, data.password)
+        .typeText(page.txt_frm_address, data.pbox)
+
+        //Checking Address field
+        .expect(page.txt_frm_address.value).contains(data.pbox)
+        .takeScreenshot()
+
+    await t
+        //Writing City and State
+        .typeText(page.txt_frm_city, data.city)
+        .click(page.drp_frm_state)
+        .click(page.drp_frm_state.find('option').withText(data.state))
+
+        //Checking City field
+        .expect(page.txt_frm_city.value).eql(data.city)
+
+        //Writing Zip code, Country, Mobile Phone and Alias
+        .typeText(page.txt_frm_zip_Code, data.zipCode)
+        .click(page.drp_frm_country)
+        .click(page.drp_frm_country.find('option').withText(data.country))
+        .typeText(page.txt_frm_mobile_Phone, data.mobilePhone)
+        .typeText(page.txt_frm_alias, data.alias, {replace: true})
+
+        //Checking Zip code, Country, Mobile Phone and Alias
+        .expect(page.txt_frm_zip_Code.value).eql(data.zipCode)
+        .expect(page.txt_frm_mobile_Phone.value).eql(data.mobilePhone)
+        .expect(page.txt_frm_alias.value).eql(data.alias)
+        .takeScreenshot()
+
+        //click on 'Register'
+        .click(page.btn_register)
+
+    await t
+        .wait(1000)
+        .takeScreenshot()
+
+        //Check user Name in Navigation Bar
+        .expect(page.NavBar_Username.innerText).eql(data.firstName + ' ' + data.lastName)
+        .expect(page.NavBar_Username.exists).ok()
+
+        //Sign Out
+        .click(page.NavBar_SignOut)
+});
