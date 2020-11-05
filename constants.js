79ae7953aee84aee73881412fb6e338c663811f1
PedroB <pedrobarragan30@gmail.com>
2020-11-04 21:59:05 -0600

    Adding Constants file

---
+++ constants.js
@@ -0,0 +1,15 @@
+
+let constants = {
+    msg_invalidEmail:           "Invalid email address.",
+    msg_eigthErrors:            "There are 8 errors",
+    msg_emaiRequired:           "An email address required.",
+    msg_pswdRequired:           "Password is required.",
+    url_categories:             "id_category=",
+    url_women:                  "id_category=3",
+    url_dresses:                  "id_category=8",
+    url_tshirt:                  "id_category=5",
+    int_expect001:              1,
+    int_expect007:              7
+}
+
+export {constants}
