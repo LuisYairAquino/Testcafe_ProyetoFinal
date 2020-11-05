47ec6c05fc6deb0eee8bda340fadb390767fa030
PedroB <pedrobarragan30@gmail.com>
2020-11-04 21:59:44 -0600

    Adding new values

--- data.js
+++ data.js
@@ -2,21 +2,22 @@
 let rndNumber = Math.random().toString(36).substr(1,4);
 
 let data = {
-    email: 'dummy' + rndNumber + '@mailinator.com',
-    bad_email: "bad@mail",
-    firstName: "Juan",
-    lastName: "Camaney",
-    password: "123456",
-    pbox: "Somewhere underthe Rainbow 1234",
-    city: "Mobile",
-    state: "Alabama",
-    zipCode: "36602",
-    country: "United States",
-    mobilePhone: "219892828492",
-    alias : "My Home",
-    registeredEmail: 'dummy.nie@mailinator.com',
-    inExistentItem: 'Dressing',
-    ExistentItem: 'Blouse'
+    email:              'dummy' + rndNumber + '@mailinator.com',
+    bad_email:          "bad@mail",
+    firstName:          "Juan",
+    lastName:           "Camaney",
+    password:           "123456",
+    pbox:               "Somewhere underthe Rainbow 1234",
+    city:               "Mobile",
+    state:              "Alabama",
+    zipCode:            "36602",
+    country:            "United States",
+    mobilePhone:        "219892828492",
+    alias :             "My Home",
+    registeredEmail:    'dummy.nie@mailinator.com',
+    nonExistingItem:     'Dressing',
+    existingItem01:     'Blouse',
+    existingItem02:     'Dress'
 }
 
 export {data}
